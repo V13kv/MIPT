10f1df55e783c6753731ea3896012e029942af90
V13kv <89425316+V13kv@users.noreply.github.com>
2021-09-15 23:59:25 +0300

    Add files via upload

--- First Course/First Semester/Onegin/include/text.h
+++ First Course/First Semester/Onegin/include/text.h
@@ -8,30 +8,37 @@ typedef struct
     char *const data;
     const int capacity;
     const int lines_count;
-} text_t;
+} text_st;
 
-text_t getTextObject(FILE *fs);
-void freeTextObject(text_t text);
+text_st getTextObject(FILE *fs);
+void freeTextObject(text_st text);
 
 int getTextSize(FILE *fs);
 int getTotalAmountOfLines(char *text_data);
 void deleteEmptyLines(char *text);
-void printTextObject(text_t text);
+void printTextObject(text_st text);
 
-//int getAmountOfNonEmptyLines(FILE *fs);
-
-
-typedef struct 
+typedef struct TEXT_LINES
 {
     char *beginning;
     int length;
-} text_lines_t;
+} text_lines_st;
 
-text_lines_t* getTextLinesObject(text_t text);
-void freeTextLinesObject(text_lines_t *lines);
+typedef enum LINE_COMPARISON_RESULTS
+{
+    SECOND_LINE_IS_LOWER,
+    SECOND_LINE_IS_GREATER,
+    LINES_ARE_EQUAL
+} cmp_res_et;
 
-void printSeveralTextLines(text_lines_t *lines, int lines_to_print);
-void lexicographicTextLinesSort(text_lines_t *lines, const int lines_count);
+text_lines_st* getTextLinesObject(text_st text);
+void freeTextLinesObject(text_lines_st *lines);
+void saveTextLinesObject(text_lines_st *lines, int count_lines, FILE *fs);
+
+void printSeveralTextLines(text_lines_st *lines, int lines_to_print, int total_lines);
+void lexicographicTextLinesSort(text_lines_st *lines, const int lines_count);
+cmp_res_et lexicographicComparison(text_lines_st a, text_lines_st b);
+void swap(text_lines_st *line1, text_lines_st *line);
 
 
 #endif  // TEXT_H
