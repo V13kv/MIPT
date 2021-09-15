df503ac10c7eb610915738d50aba3bbff8977d30
V13kv <89425316+V13kv@users.noreply.github.com>
2021-09-15 19:53:32 +0300

    Add files via upload

--- First Course/First Semester/Onegin/include/text.h
+++ First Course/First Semester/Onegin/include/text.h
@@ -3,9 +3,35 @@
 
 #include <stdio.h>
 
+typedef struct 
+{
+    char *const data;
+    const int capacity;
+    const int lines_count;
+} text_t;
+
+text_t getTextObject(FILE *fs);
+void freeTextObject(text_t text);
 
 int getTextSize(FILE *fs);
-int getTotalAmountOfLines(char *text_data, int text_size);
+int getTotalAmountOfLines(char *text_data);
+void deleteEmptyLines(char *text);
+void printTextObject(text_t text);
+
+//int getAmountOfNonEmptyLines(FILE *fs);
+
+
+typedef struct 
+{
+    char *beginning;
+    int length;
+} text_lines_t;
+
+text_lines_t* getTextLinesObject(text_t text);
+void freeTextLinesObject(text_lines_t *lines);
+
+void printSeveralTextLines(text_lines_t *lines, int lines_to_print);
+void lexicographicTextLinesSort(text_lines_t *lines, const int lines_count);
 
 
 #endif  // TEXT_H
