0dfa35a4dc8a01e74db4404e270ed3e7762163e1
V13kv <89425316+V13kv@users.noreply.github.com>
2021-09-17 11:53:23 +0300

    Everything is implemented except my_qsort

--- First Course/First Semester/Onegin/include/lines.h
+++ First Course/First Semester/Onegin/include/lines.h
@@ -3,6 +3,8 @@
 
 
 #include <stdio.h>
+#include <stdbool.h>
+
 #include "../include/constants.h"
 
 typedef struct
@@ -16,19 +18,14 @@ void saveTextLinesObject(text_lines_st *lines, int count_lines, FILE *fs);
 void freeTextLinesObject(text_lines_st *lines);
 void printSeveralTextLines(text_lines_st *lines, int lines_to_print, int total_lines);
 
-//TODO: optimize
-int normalComparison(const void* a, const void* b);
-int reversedComparison(const void *a, const void *b);
 
+bool isLetter(char chr);
+int directLinesComparison(const void* a, const void* b);
+void my_strrev(char *str, int len);
+int reversedLinesComparison(const void *a, const void *b);
+
+void swap(text_lines_st *line1, text_lines_st *line2);
 //TODO: void my_qsort(text_lines_st *lines, int low, int high, int (*comp)(const void *, const void *));
-///TODO: void swap(text_lines_st *line1, text_lines_st *line2);
-
-
-/*
-void lexicographicTextLinesSort(text_lines_st *lines, const int lines_count);
-cmp_res_et lexicographicComparison(text_lines_st a, text_lines_st b);
-void swap(text_lines_st *line1, text_lines_st *line);
-*/
 
 
 #endif  // LINES_H
