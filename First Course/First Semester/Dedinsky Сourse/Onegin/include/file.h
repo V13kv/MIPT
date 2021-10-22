aae69d787957a2407fe9463609cd1f512b9e1000
V13kv <89425316+V13kv@users.noreply.github.com>
2021-10-23 01:39:52 +0300

    Add files via upload

---
+++ First Course/First Semester/Dedinsky Сourse/Onegin/include/file.h
@@ -0,0 +1,16 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include <stdio.h>
+
+
+/**
+ * @brief Get the capacity of the text in the file
+ * 
+ * @param fs 
+ * @return int 
+ */
+int get_file_capacity(FILE *fs);
+
+
+#endif  // FILE_H
--- First Course/First Semester/Dedinsky Сourse/Onegin/include/text.h
+++ First Course/First Semester/Dedinsky Сourse/Onegin/include/text.h
@@ -3,7 +3,23 @@
 
 #include <stdio.h>
 
-//TODO: craete file.h and combine text.h and lines.h
+enum class FILE_MODE
+{
+    R,
+    RB,
+    A,
+    W
+};
+
+/**
+ * @brief Structure represents each line of the text in a file
+ * 
+ */
+typedef struct
+{
+    char *beginning;
+    size_t length;
+} text_line_t;
 
 /**
  * @brief Structure represents the text in a file
@@ -14,37 +30,32 @@ typedef struct
     char *data;
     size_t size;
     int lines_count;
-} text_st;  // TODO: buffer_st, text_st, text_line_st embedded structures (one structure)
+    text_line_t *lines;
+} text_t;  // TODO: buffer_st, text_t, text_line_t embedded structures (one structure)
 
 /**
- * @brief Get a text structure representing the text of the file with the calculated values (see text_st structure)
+ * @brief Get a text structure representing the text of the file with the calculated values (see text_t structure)
  * 
  * @param text 
  * @param file_path 
  */
-void text_st_constructor(text_st *text, const char *file_path);
+void textCtor(text_t *text, const char *file_path, const FILE_MODE mode);
 
-/**
- * @brief Get the capacity of the text in the file
- * 
- * @param fs 
- * @return int 
- */
-int get_file_capacity(FILE *fs);
+char *getStrMode(const FILE_MODE mode);
 
 /**
  * @brief Remove blank lines from a buffer
  * 
  * @param text 
  */
-void delete_empty_lines(char *text);
+void deleteEmptyLines(char *text);
 
 /**
  * @brief Converts lines ending in '\n' to C-strings ending in '\0'
  * 
  * @param text 
  */
-void convert_lines_to_CStrings(char *text);
+void convertLinesToCStrings(char *text);
 
 /**
  * @brief Get the total amount of lines in a buffer
@@ -52,7 +63,7 @@ void convert_lines_to_CStrings(char *text);
  * @param text 
  * @return int 
  */
-int get_total_amount_of_lines(char *text_data);
+int getTotalAmountOfLines(char *text_data);
 
 /**
  * @brief Print basic information about the structure including first 'symbols_to_print' symbols
@@ -60,14 +71,39 @@ int get_total_amount_of_lines(char *text_data);
  * @param text 
  * @param symbols_to_print 
  */
-void text_st_print(text_st *text, size_t symbols_to_print);
+void printTextObject(text_t *text, size_t symbols_to_print);
 
 /**
  * @brief Free the memory allocated for the text of the file
  * 
  * @param text 
  */
-void text_st_deconstructor(text_st *text);
+void textDtor(text_t *text);
+
+/**
+ * @brief Extract lines from text using lines structure
+ * 
+ * @param text 
+ * @return text_line_t* 
+ */
+text_line_t* getTextLines(text_t *text);
+
+/**
+ * @brief Writing line objects to file
+ * 
+ * @param text 
+ * @param file_name 
+ * @param mode 
+ */
+void exportTextObject(const text_t *const text, const char *file_name, const FILE_MODE mode);
+
+/**
+ * @brief Printing 'lines_to_print' lines from the beginning of the text
+ * 
+ * @param text 
+ * @param lines_to_print 
+ */
+void printTextLines(const text_t *const text, const int lines_to_print);
 
 
 #endif  // TEXT_H
