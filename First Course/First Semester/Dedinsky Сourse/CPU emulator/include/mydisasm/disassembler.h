65ecc3207d1ce9436b74da230ea44600c62c0449
V13kv <89425316+V13kv@users.noreply.github.com>
2021-10-24 17:54:16 +0300

    Add files via upload

--- First Course/First Semester/Dedinsky Сourse/CPU emulator/include/mydisasm/disassembler.h
+++ First Course/First Semester/Dedinsky Сourse/CPU emulator/include/mydisasm/disassembler.h
@@ -10,7 +10,23 @@
 #include "../commands.h"
 #include "../../lib/text/include/text.h"
 
+#define GET_MNEMONICS(opcode) commands_table[opcode].mnemonics
+#define GET_ENCODED_COMMAND_OPCODE(encodedCommand) (encodedCommand & 0xF0) >> 4
+#define GET_ENCODED_COMMAND_ARGS_COUNT(encodedCommand) (encodedCommand & 0xC) >> 2
 
+//TODO: own command_t structure for disassembly
+//TODO: move enum class to general.h header file
+enum class ASM_EXIT_CODE
+{
+    BAD_COMMAND_STRUCTURE_FIELDS,
+    NO_SUCH_MNEMONICS_FOUND,
+};
+
+EXIT_CODES disassembly(text_t *code, const char *const output_file_name);
+EXIT_CODES decodeCommand(char encodedCommand, command_t *command);
+EXIT_CODES decodeCommandArg(char encodedArgument, char *decodedArgument);
+
+EXIT_CODES exportDecodedCommand(command_t *command, FILE *fs);
 
 
 
